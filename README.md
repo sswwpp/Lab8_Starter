343eb648adeb19fc1a01c1f71872dd0c772104be
Sebastian <ian.pamudji@gmail.com>
2022-11-21 23:21:51 -0800

    finished

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Lab 8 - Starter
+
+Sebastian Pamudji, working alone
+
+## Check your understanding
+
+1. Within a Github actions, because it checks for any inconsistencies or errors whenever there is a push to ensure nothing breaks within the bigger code structure.
+2. No
+3. No, since a feature is composed of multiple functions that can be unpredictable at times. We can not determine what is exactly supposed to happen given the amount of variability.
+4. Yes, since message length is something we can predict whether it is true or false, we can create a simple test to check that specifically.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -1,5 +1,69 @@
 // unit.test.js
 
-const functions = require('../code-to-unit-test/unit-test-me.js');
+const functions = require("../code-to-unit-test/unit-test-me.js");
 
 // TODO - Part 2
+test("isPhoneNumber a", () => {
+  expect(functions.isPhoneNumber("0")).toBe(false);
+});
+test("isPhoneNumber 123", () => {
+  expect(functions.isPhoneNumber("0000000000")).toBe(false);
+});
+test("isPhoneNumber 000-000-0000", () => {
+  expect(functions.isPhoneNumber("000-000-0000")).toBe(true);
+});
+test("isPhoneNumber (000)000-0000", () => {
+  expect(functions.isPhoneNumber("(000)000-0000")).toBe(true);
+});
+
+test("isEmail @@@@", () => {
+  expect(functions.isEmail("@@@@")).toBe(false);
+});
+test("isEmail test", () => {
+  expect(functions.isEmail("test")).toBe(false);
+});
+test("isEmail test@gmail.com", () => {
+  expect(functions.isEmail("test@gmail.com")).toBe(true);
+});
+test("isEmail student@ucsd.edu", () => {
+  expect(functions.isEmail("student@ucsd.edu")).toBe(true);
+});
+
+test("isStrongPassword 0test", () => {
+  expect(functions.isStrongPassword("0test")).toBe(false);
+});
+test("isStrongPassword no", () => {
+  expect(functions.isStrongPassword("no")).toBe(false);
+});
+test("isStrongPassword strong0011", () => {
+  expect(functions.isStrongPassword("strong0011")).toBe(true);
+});
+test("isStrongPassword stronger_0312", () => {
+  expect(functions.isStrongPassword("stronger_0312")).toBe(true);
+});
+
+test("isDate 0000", () => {
+  expect(functions.isDate("0000")).toBe(false);
+});
+test("isDate 13/17/300092", () => {
+  expect(functions.isDate("13/17/300092")).toBe(false);
+});
+test("isDate 11/21/2022", () => {
+  expect(functions.isDate("11/21/2022")).toBe(true);
+});
+test("isDate 1/1/2001", () => {
+  expect(functions.isDate("1/1/2001")).toBe(true);
+});
+
+test("isHexColor 0", () => {
+  expect(functions.isHexColor("0")).toBe(false);
+});
+test("isHexColor aa", () => {
+  expect(functions.isHexColor("aa")).toBe(false);
+});
+test("isHexColor ffffff", () => {
+  expect(functions.isHexColor("ffffff")).toBe(true);
+});
+test("isHexColor #000000", () => {
+  expect(functions.isHexColor("#000000")).toBe(true);
+});
